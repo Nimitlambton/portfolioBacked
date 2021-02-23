69159e6ebcbc67c436558e2fe573534c5ef08da3
Nimitpamnani <C0764944@mylambton.ca>
2021-02-23 22:36:46 +0530

    witProjectlist

--- src/api.js
+++ src/api.js
@@ -24,6 +24,51 @@ router.get("/jumbtron", (req, res) => {
   });
 });
 
+router.get("/projects", (req, res) => {
+  res.json({
+    projectsList: [
+      {
+        title: "MemeNator",
+        projectId: 1,
+        desc:
+          "This is my first project, that is build on React.js. It can be used to generate cool meme and edit images",
+        repolink: "https://github.com/Nimitlambton/MN",
+        DeployedLink: "https://meme-nator.netlify.app",
+        thumbnail: Background,
+        status: "completed",
+      },
+      {
+        title: "Personal portfolio",
+        status: "completed",
+        projectId: 2,
+        desc:
+          "This is react based responsive Web App , used to showcase all the projects that are created till now ",
+        repolink: "https://github.com/Nimitlambton/NimitpamnaniProfolio",
+        DeployedLink: "https://nimitpamnani.rocks/#home",
+        thumbnail: protfolioImg,
+      },
+      {
+        title: "The Weather App",
+        projectId: 3,
+        desc:
+          "This is weatherApp based on React.js , it can be used to fetch weather of any City in the world 🌏 ",
+        repolink: "https://github.com/Nimitlambton/weatherApp",
+        thumbnail: Weather,
+        DeployedLink: "https://nimit-theweatherapp.netlify.app/",
+        status: "completed",
+      },
+      {
+        title: "MemeNator",
+        projectId: 4,
+        desc: "This is my first project, that is build on React.js",
+        thumbnail: "",
+        repolink: "",
+        status: "",
+      },
+    ],
+  });
+});
+
 app.use("/.netlify/functions/api", router);
 
 module.exports.handler = serverless(app);
