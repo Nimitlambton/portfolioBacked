9cce23ade877acd1df17b3cf879b0943420a07f2
Nimitpamnani <C0764944@mylambton.ca>
2021-06-07 19:18:13 -0400

    withImageBackendSupport

--- src/api.js
+++ src/api.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
+var fs = require("fs");
 const serverless = require("serverless-http");
 var cors = require("cors");
-
 const app = express();
-
 const router = express.Router();
 
 app.use(cors());
@@ -34,8 +32,12 @@ router.get("/projects", (req, res) => {
           "This is my first project, that is build on React.js. It can be used to generate cool meme and edit images",
         repolink: "https://github.com/Nimitlambton/MN",
         DeployedLink: "https://meme-nator.netlify.app",
-        thumbnail: "",
 
+        //this function passes  url of a image that are kept in assest folder  which further converts image into base64 data.
+        thumbnail: base64_encode(
+          __dirname +
+            "Users/nimi/Desktop/javascriptws/reactproj/porfolinbackend/portfolioBack/src/Assests/freelance.png"
+        ),
         status: "completed",
       },
       {
@@ -46,9 +48,12 @@ router.get("/projects", (req, res) => {
           "This is react based responsive Web App , used to showcase all the projects that are created till now ",
         repolink: "https://github.com/Nimitlambton/NimitpamnaniProfolio",
         DeployedLink: "https://nimitpamnani.rocks/#home",
-        thumbnail: `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
-        AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
-            9TXL0Y4OHwAAAABJRU5ErkJggg==`,
+
+        //this function passes  url of a image that are kept in assest folder  which further converts image into base64 data.
+        thumbnail: base64_encode(
+          __dirname +
+            "Users/nimi/Desktop/javascriptws/reactproj/porfolinbackend/portfolioBack/src/Assests/portfolio.png"
+        ),
       },
       {
         title: "The Weather App",
@@ -56,9 +61,12 @@ router.get("/projects", (req, res) => {
         desc:
           "This is weatherApp based on React.js , it can be used to fetch weather of any City in the world 🌏 ",
         repolink: "https://github.com/Nimitlambton/weatherApp",
-        thumbnail: `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
-        AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
-            9TXL0Y4OHwAAAABJRU5ErkJggg==`,
+
+        //this function passes  url of a image that are kept in assest folder  which further converts image into base64 data.
+        thumbnail: base64_encode(
+          __dirname +
+            "Users/nimi/Desktop/javascriptws/reactproj/porfolinbackend/portfolioBack/src/Assests/eg.png"
+        ),
         DeployedLink: "https://nimit-theweatherapp.netlify.app/",
         status: "completed",
       },
@@ -73,6 +81,17 @@ router.get("/projects", (req, res) => {
     ],
   });
 });
+
+//function coverts image that are kept in Assests folder data top base64
+function base64_encode(file) {
+  // read binary data
+  var bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+
+  var abc = new Buffer(bitmap).toString("base64");
+
+  return "data:image/png;base64," + abc;
+}
 
 app.use("/.netlify/functions/api", router);
 
