1800ce987dfe4994685ef4a6c98e061d158c1ed4
Nimitpamnani <C0764944@mylambton.ca>
2021-06-08 18:14:10 -0400

    try3

--- src/api.js
+++ src/api.js
@@ -34,10 +34,10 @@ router.get("/projects", (req, res) => {
         DeployedLink: "https://meme-nator.netlify.app",
 
         //this function passes  url of a image that are kept in assest folder  which further converts image into base64 data.
-        // thumbnail: base64_encode(
-        //   __dirname +
-        //     "Users/nimi/Desktop/javascriptws/reactproj/porfolinbackend/portfolioBack/src/Assests/freelance.png"
-        // ),
+        thumbnail: base64_encode(
+          __dirname +
+            "Users/nimi/Desktop/javascriptws/reactproj/porfolinbackend/portfolioBack/src/Assests/freelance.png"
+        ),
         status: "completed",
       },
       // {
