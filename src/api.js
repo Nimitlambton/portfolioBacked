928474b0a1099375c3509a200efac1f1c53c144b
Nimitpamnani <C0764944@mylambton.ca>
2021-06-08 18:07:28 -0400

    intest

--- src/api.js
+++ src/api.js
@@ -14,6 +14,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/jumbtron", (req, res) => {
+  console.warn("helloworld");
   res.json({
     h1: "Hi 👋 , Thanks for Stoping by",
     p: "Currently learning MERN Stack & ReactNative ",
